fcc802bade54f796ec7de61fff1c8a408caba515
AkshaySodhi <95957791+AkshaySodhi@users.noreply.github.com>
2024-06-25 23:56:29 +0530

    frontend done

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,18 +1,34 @@
 import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../../skeletons/MessageSkeleton";
+import { useEffect, useRef } from "react";
 
 const Messages = () => {
+  const { loading, messages } = useGetMessages();
+
+  const lastMsgRef = useRef();
+  useEffect(() => {
+    setTimeout(
+      () => lastMsgRef.current?.scrollIntoView({ behavior: "smooth" }),
+      100
+    );
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((m) => (
+          <div key={m._id} ref={lastMsgRef}>
+            <Message message={m} />
+          </div>
+        ))}
+
+      {loading && [...Array(3)].map((_, ind) => <MessageSkeleton key={ind} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the convo</p>
+      )}
     </div>
   );
 };
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,19 @@
 import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((c, ind) => (
+        <Conversation
+          key={c._id}
+          conversation={c}
+          lastInd={ind === conversations.length - 1}
+        />
+      ))}
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   );
 };
