6d37410c3b7270ffa7b17e41f4c362f84faeaec5
AkshaySodhi <95957791+AkshaySodhi@users.noreply.github.com>
2024-07-04 21:24:01 +0530

    deployment-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-import SignUp from "./pages/signup/Signup.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
 import { useAuthContext } from "./context/AuthContext.jsx";
 
 function App() {
